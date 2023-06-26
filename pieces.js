576b63c5e8f1886eb21f23e4b2b4a7488e9ed493
Samuel-Jr Ndolou <samuel-jr@MBPdeSamuelJr.lan>
2023-06-26 16:04:01 +0200

    affichage complet des fiches produit

--- pieces.js
+++ pieces.js
@@ -4,37 +4,86 @@ const pieces = await reponse.json();
 
 
 
-// Création des balises avant affichage 
-const article = pieces[0];
+// // Création des balises avant affichage 
+// const article = pieces[0];
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image; 
+// const imageElement = document.createElement("img");
+// imageElement.src = article.image; 
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+// const nomElement = document.createElement("h2");
+// nomElement.innerText = article.nom;
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`; // condition pour l'indicateur de  prix 
+// const prixElement = document.createElement("p");
+// prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`; // condition pour l'indicateur de  prix 
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? ("aucune catégorie");
+// const categorieElement = document.createElement("p");
+// categorieElement.innerText = article.categorie ?? ("aucune catégorie");
 
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? ("Pas de description pour le moment."); // condition en cas de non description
+// const descriptionElement = document.createElement("p");
+// descriptionElement.innerText = article.description ?? ("Pas de description pour le moment."); // condition en cas de non description
 
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = `${article.disponibilite === true ? "En stock" : "Rupture de stock"}`; // condition pour la gestion des stocks 
+// const disponibiliteElement = document.createElement("p");
+// disponibiliteElement.innerText = `${article.disponibilite === true ? "En stock" : "Rupture de stock"}`; // condition pour la gestion des stocks 
   
 
 
-// ratachement des élément au DOM avec le parent qui permet l'affichage 
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild (imageElement);
-sectionFiches.appendChild (nomElement);
-sectionFiches.appendChild (prixElement);
-sectionFiches.appendChild (categorieElement);
-sectionFiches.appendChild (descriptionElement);
-sectionFiches.appendChild (disponibiliteElement);
+// // ratachement des élément au DOM avec le parent qui permet l'affichage 
+// const sectionFiches = document.querySelector(".fiches");
+// sectionFiches.appendChild (imageElement);
+// sectionFiches.appendChild (nomElement);
+// sectionFiches.appendChild (prixElement);
+// sectionFiches.appendChild (categorieElement);
+// sectionFiches.appendChild (descriptionElement);
+// sectionFiches.appendChild (disponibiliteElement);
 
 
 
+
+
+   
+    for (let i = 0; i < pieces.length; i++) {
+
+        // Récupération de l'élément du DOM qui accueillera les fiches
+        const sectionFiches = document.querySelector(".fiches");
+        // Création d’une balise dédiée à une pièce automobile
+        const pieceElement = document.createElement("article");
+
+        // On crée l’élément img.
+        const imageElement = document.createElement("img");
+        // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
+        imageElement.src = pieces[i].image;
+
+        // Idem pour le nom, le prix et la catégorie...
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = pieces[i].nom;
+
+        const prixElement = document.createElement("p");
+        prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`; // condition pour l'indicateur de  prix 
+
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = pieces[i].categorie;
+
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = pieces[i].description;
+
+        const disponibiliteElement = document.createElement("p");
+        disponibiliteElement.innerText =  `${pieces[i].disponibilite === true ? "En stock" : "Rupture de stock"}`; // condition pour la gestion des stocks 
+
+
+
+
+
+        // On rattache la balise article à la section Fiches
+        sectionFiches.appendChild(pieceElement);
+
+        // On rattache l’image à pieceElement (la balise article)
+        pieceElement.appendChild(imageElement);
+
+        // Idem pour le nom, le prix et la catégorie...
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(disponibiliteElement);
+
+        };
